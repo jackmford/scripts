be74f4b5a76ac6c8d3a1dc8320424ca49c531bb9
jackmford <jackmitchellfordyce@gmail.com>
2025-03-25 18:29:43 -0500

    boilerplate

--- instapaper-backup/instapaper-backup-go/main.go
+++ instapaper-backup/instapaper-backup-go/main.go
@@ -2,8 +2,42 @@ package main
 
 import (
   "fmt"
+  "os"
 )
+
+var CONSUMER_KEY string = os.Getenv("INSTAPAPER_KEY")
+var CONSUMER_SECRET string = os.Getenv("INSTAPAPER_SECRET")
+var USERNAME string = os.Getenv("INSTAPAPER_USERNAME")
+var PASSWORD string = os.Getenv("INSTAPAPER_PASSWORD")
+
+type Bookmark struct {
+  Title string
+  url string
+  index int
+}
+
+func get_access_token () (string, string) {
+  return "", ""
+}
+
+func get_bookmarks(access_token string, access_token_secret string) []Bookmark {
+
+  return nil
+}
 
 func main() {
   fmt.Println("Hi")
+
+  var access_token string
+  var access_token_secret string
+  var bookmarks []Bookmark
+
+  access_token, access_token_secret = get_access_token() 
+  bookmarks = get_bookmarks(access_token, access_token_secret)
+
+  for _, bookmark := range bookmarks {
+    fmt.Println(bookmark.Title)
+  }
+
+  fmt.Println(CONSUMER_KEY)
 }

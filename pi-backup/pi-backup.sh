cc0a302aca83e1f714315a4c7cebbd991cd6c361
jackmford <jackmitchellfordyce@gmail.com>
2025-04-17 12:42:31 -0500

    removing dupe backup adding prints

--- pi-backup/pi-backup.sh
+++ pi-backup/pi-backup.sh
@@ -4,12 +4,15 @@
 PI_USER=jackmf
 PI_HOST=backup-pi.local
 PI_DEST="/mnt/backupdrive/backups/laptop"
+RSYNC_IGNORE="/Users/jackfordyce/Lab/git-repos/scripts/pi-backup/rsync-ignore.txt"
 
 echo "Starting backup..."
 
-rsync -avh --delete --exclude-from=rsync-ignore.txt ~/Lab $PI_USER@$PI_HOST:$PI_DEST/
-rsync -avh --delete --exclude-from=rsync-ignore.txt ~/Lab $PI_USER@$PI_HOST:$PI_DEST/
-rsync -avh --delete --exclude-from=rsync-ignore.txt ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Vault $PI_USER@$PI_HOST:$PI_DEST/
+echo "Backing up... ~/Lab"
+rsync -avh --delete --exclude-from=$RSYNC_IGNORE ~/Lab $PI_USER@$PI_HOST:$PI_DEST/
+
+echo "Backing up... Obsidian Vault"
+rsync -avh --delete --exclude-from=$RSYNC_IGNORE ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Vault $PI_USER@$PI_HOST:$PI_DEST/
 
 echo "Backup complete."
 

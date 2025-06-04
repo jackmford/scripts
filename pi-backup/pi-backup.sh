5689fbdbdaf0aacbdbe0cc0b1c3322625efc5744
jackmford <jackmitchellfordyce@gmail.com>
2025-06-03 19:23:35 -0500

    few fixups

--- pi-backup/pi-backup.sh
+++ pi-backup/pi-backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Replace this with your actual Pi IP or hostname if set
 PI_USER=jackmf
 PI_HOST=backup-pi.local
 PI_DEST="/mnt/backupdrive/backups/laptop"
@@ -9,10 +8,13 @@ RSYNC_IGNORE="/Users/jackfordyce/Lab/git-repos/scripts/pi-backup/rsync-ignore.tx
 echo "Starting backup..."
 
 echo "Backing up... ~/Lab"
-rsync -avh --delete --exclude-from=$RSYNC_IGNORE ~/Lab $PI_USER@$PI_HOST:$PI_DEST/
+rsync -avh --delete --partial --append-verify --exclude-from=$RSYNC_IGNORE ~/Lab $PI_USER@$PI_HOST:$PI_DEST/
 
 echo "Backing up... Obsidian Vault"
-rsync -avh --delete --exclude-from=$RSYNC_IGNORE ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Vault $PI_USER@$PI_HOST:$PI_DEST/
+rsync -avh --delete --partial --append-verify --exclude-from=$RSYNC_IGNORE ~/Library/Mobile\ Documents/iCloud~md~obsidian/Documents/Obsidian\ Vault $PI_USER@$PI_HOST:$PI_DEST/
+
+echo "Backing up... iCloud Drive"
+rsync -avh --delete --partial --append-verify --exclude-from=$RSYNC_IGNORE ~/Library/Mobile\ Documents/com~apple~CloudDocs $PI_USER@$PI_HOST:$PI_DEST/
 
 echo "Backup complete."
 
